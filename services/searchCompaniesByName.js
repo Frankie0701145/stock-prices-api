5c99d2b4e61d6bab1e978282598248526f7c3a37
Francis <francis@senga.co>
2023-03-28 23:25:57 +0300

    Add the return of symbol for the two searches.

--- services/searchCompaniesByName.js
+++ services/searchCompaniesByName.js
@@ -2,7 +2,7 @@ const db = require('./db');
 const helper = require('../helpers');
 
 async function searchCompanyByName(searchVal){
-    const rows = await db.query("SELECT company_id, company_name FROM StockPrice WHERE MATCH (company_name) AGAINST (?)", [searchVal]);
+    const rows = await db.query("SELECT company_id, company_name, symbol FROM StockPrice WHERE MATCH (company_name) AGAINST (?)", [searchVal]);
     const data = helper.emptyOrRows(rows);
     return {
         data
--- services/searchCompaniesByTicker.js
+++ services/searchCompaniesByTicker.js
@@ -2,7 +2,7 @@ const db = require('./db');
 const helper = require('../helpers');
 
 async function searchCompanyByTicker(searchVal){
-    const rows = await db.query("SELECT company_id, company_name FROM StockPrice WHERE MATCH (symbol) AGAINST (?)", [searchVal]);
+    const rows = await db.query("SELECT company_id, company_name, symbol FROM StockPrice WHERE MATCH (symbol) AGAINST (?)", [searchVal]);
     const data = helper.emptyOrRows(rows);
     return {
         data
