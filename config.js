cccd061431e2c90f18a33a934690761e9904b2db
Francis <francis@senga.co>
2023-03-28 23:15:20 +0300

    Switch to using pool of connection for mysql.

--- config.js
+++ config.js
@@ -4,7 +4,12 @@ const config = {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD, 
       database: process.env.DB_NAME,
-      port: process.env.DB_PORT
+      port: process.env.DB_PORT,
+      waitForConnections: true,
+      connectionLimit: 10,
+      maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+      idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+      queueLimit: 0
     },
 };
 
--- services/db.js
+++ services/db.js
@@ -1,9 +1,11 @@
-const mysql = require('mysql2/promise');
+const mysql = require('mysql2');
 const config = require('../config');
 
 async function query(sql, params) {
-    const connection = await mysql.createConnection(config.db);
-    const [results, ] = await connection.execute(sql, params);
+    // const connection = await mysql.createConnection(config.db);
+    const pool = mysql.createPool(config.db);
+    const promisePool = pool.promise();
+    const [results, ] = await promisePool.query(sql, params);
   
     return results;
 }
